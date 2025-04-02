d18dc66c6a0b9955082ef6bffffaa6977975a83a
anthony kiarie <akiarie27@gmail.com>
2025-04-02 18:02:32 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+// Initialize the cats array with the required values
+var cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions that modify the original array
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+// Non-destructive functions that return a new array without modifying the original
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
